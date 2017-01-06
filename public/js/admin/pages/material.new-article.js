96fb2c7cb30c12aea3e50e9001ba80626e32f34e
wjcgithub <wjc163@sina.cn>
2017-01-06 18:38:12 +0800

    添加图文预览功能

--- public/js/admin/pages/material.new-article.js
+++ public/js/admin/pages/material.new-article.js
@@ -3,22 +3,22 @@
  *
  * @author overtrue <anzhengchao@gmail.com>
  */
-define(['jquery', 'uploader', 'util', 'repos/article-store', 'admin/common','layer'], function ($, Uploader, Util, Article) {
-    $(function(){
+define(['jquery', 'uploader', 'util', 'repos/article-store', 'admin/common', 'layer'], function ($, Uploader, Util, Article) {
+    $(function () {
         var $ue = UE.getEditor('container');
         var $form = $('.article-form');
         var $previewItemTemplate = _.template($('#preview-item-template').html());
         var $firstItem = $('.article-preview-item.first');
-        $ue.ready(function() {
+        $ue.ready(function () {
             // $ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
             // 初始化第一张图片
             $firstItem.find('a.edit').click();
         });
         //检查是否显示添加按钮
-        function performAddBtn () {
+        function performAddBtn() {
             var $addBtnBox = $('.add-new-item').closest('.article-preview-item');
 
-            if ($('.articles-preview-container .article-preview-item').length -1 >= 8) {
+            if ($('.articles-preview-container .article-preview-item').length - 1 >= 8) {
                 $addBtnBox.slideUp(100);
             } else {
                 $addBtnBox.slideDown(100);
@@ -26,18 +26,18 @@ define(['jquery', 'uploader', 'util', 'repos/article-store', 'admin/common','lay
         }
 
         // 根据属性渲染 form
-        function renderForm ($attributes) {
+        function renderForm($attributes) {
             console.log($attributes);
             // 必须从表单字段开始遍历
             var $keys = Util.parseForm($form);
 
             for ($attribute in $keys) {
-                $form.find('[name='+$attribute+']').val($attributes[$attribute]);
+                $form.find('[name=' + $attribute + ']').val($attributes[$attribute]);
             }
 
             if ($attributes['content']) {
                 $ue.setContent($attributes['content']);
-            }else{
+            } else {
                 $ue.setContent('');
             }
 
@@ -45,29 +45,29 @@ define(['jquery', 'uploader', 'util', 'repos/article-store', 'admin/common','lay
         }
 
         // 渲染预览框
-        function previewItem ($attributes) {
+        function previewItem($attributes) {
             var $item = $('.article-preview-item.active');
 
             $item.find('.attr-title').html($attributes['title'] || '标题');
 
-            if($attributes['cover_url']){
+            if ($attributes['cover_url']) {
                 $('.form-group-div').find('img').attr('src', $attributes['cover_url']).slideDown(100);
-            }else{
+            } else {
                 $('.form-group-div').find('img').slideUp(100);
             }
 
-            if($attributes['cover_url']){
+            if ($attributes['cover_url']) {
                 //修改实时预览
-                if($('.article-preview-item.active').hasClass('first')){
-                    $('.article-preview-item.active').find('.article-preview-item-cover-placeholder').css("backgroundImage","url('"+$attributes['cover_url']+"')");
-                }else{
-                    $('.article-preview-item.active').find('.article-preview-item-thumb-img img').attr("src",$attributes['cover_url']);
+                if ($('.article-preview-item.active').hasClass('first')) {
+                    $('.article-preview-item.active').find('.article-preview-item-cover-placeholder').css("backgroundImage", "url('" + $attributes['cover_url'] + "')");
+                } else {
+                    $('.article-preview-item.active').find('.article-preview-item-thumb-img img').attr("src", $attributes['cover_url']);
                 }
             }
         }
 
         // 保存form
-        function saveForm () {
+        function saveForm() {
             var $id = $('.article-preview-item.active').prop('id');
             var $attributes = Util.parseForm($($form));
             $attributes.content = $ue.getContent();
@@ -76,116 +76,62 @@ define(['jquery', 'uploader', 'util', 'repos/article-store', 'admin/common','lay
             previewItem($attributes);
         }
 
-        function saveAll() {
+        function saveAll(arg1) {
+            if(!arguments[0]) arg1 = "check";
             //先保存当前激活的文章
             saveForm();
-
             //循环判断每一个
             var $articles = Article.all();
             $('.alert-warning').hide();
-            var validateNum=0;
-            console.log($articles.len);
-            for($id in $articles){
-                if($articles[$id].title==''){
-                    $("#"+$id).find('a.edit').click();
-                    scroll(0,0);
+            var validateNum = 0;
+            for ($id in $articles) {
+                if ($articles[$id].title == '') {
+                    $("#" + $id).find('a.edit').click();
+                    scroll(0, 0);
                     $('.article-title').slideDown(1000);
                     break;
                 }
-                if($articles[$id].cover_url==''){
-                    $("#"+$id).find('a.edit').click();
-                    scroll(0,0);
+                if ($articles[$id].cover_url == '') {
+                    $("#" + $id).find('a.edit').click();
+                    scroll(0, 0);
                     $('.article-cover_url').slideDown(1000);
                     break;
                 }
-                if($articles[$id].content==''){
-                    $("#"+$id).find('a.edit').click();
-                    scroll(0,0);
+                if ($articles[$id].content == '') {
+                    $("#" + $id).find('a.edit').click();
+                    scroll(0, 0);
                     $('.article-content').slideDown(1000);
                     break;
                 }
                 validateNum++;
             }
-            if(validateNum == Article.getLength()){
-                alert('保存成功')
+            if (validateNum == Article.getLength()) {
+                if(typeof arg1 == 'object'){
+                    alert('保存成功')
+                }
+                return true;
             }
-        }
-        
-        // 添加项目
-        $('.articles-preview-container').on('click', '.add-new-item', function(){
-            var $parentItem = $(this).closest('.article-preview-item');
-            var $id = (new Date).getTime();
-            var $item = $($previewItemTemplate({item:{'cover_url':'/image/slt.png'}})).prop('id', $id);
 
-            $parentItem.before($item);
-
-            Article.add($id)
-
-            performAddBtn();
-        });
-
-        // 编辑项目
-        $('.articles-preview-container').on('click', 'a.edit', function(){
-
-            var $item = $(this).closest('.article-preview-item');
-
-            if ($item.hasClass('active')) { return; };
-
-            var $article = Article.get($item.prop('id'));
-            console.log('form'+$item.prop('id')+"被编辑，内容为"+$article);
-            console.log($article);
-            $item.addClass('active').siblings().removeClass('active');
-
-            renderForm($article);
-        });
-
-        // 删除项目
-        $('.articles-preview-container').on('click', 'a.delete', function(){
-            var $item = $(this).closest('.article-preview-item');
-
-            Article.delete($item.prop('id'));
-
-            $item.slideUp(200, function(){
-                $(this).remove();
-
-                performAddBtn();
-            });
-
-            window.location.reload();
-        });
-
-        var $articles = Article.all();
-
-        //初始化除了第一张的其他图片
-        for($id in $articles){
-            if ($id == 'article-first') {continue;}
-
-            if(!$articles[$id].cover_url){
-                $articles[$id].cover_url = '/image/slt.png';
-            }
-            $firstItem.after($($previewItemTemplate({item: $articles[$id]})).prop('id', $id));
+            return false;
         }
 
-        $form.on('keyup', saveForm);
-        $ue.addListener('keyup', saveForm);
-        $('.btn-save').on('click', saveAll);
 
         /**
          * 弹出选择图片提示框
          */
-        function showChoseImageDialog(obj, content){
+        function showChoseImageDialog(obj, content) {
             var _this = obj;
             layer.open({
-                title:'选择图片',
+                title: '选择图片',
                 type: 2,//1：字符串；2:content填URL
                 area: ['650px', '450px'],
-                content: content ,//这里content是一个普通的String
+                content: content,//这里content是一个普通的String
                 zIndex: layer.zIndex,
                 btn: ['确认', '取消'],
-                yes: function(layero, index){
+                yes: function (layero, index) {
                     var body = layer.getChildFrame('body', 0);
                     var size = body.find('.chose_icon').size();//选中数量
-                    if(size <= 0){
+                    if (size <= 0) {
                         layer.alert("请选择图片");
                         return;
                     }
@@ -197,14 +143,114 @@ define(['jquery', 'uploader', 'util', 'repos/article-store', 'admin/common','lay
                     layer.closeAll()
                     saveForm();
                 },
-                cancel: function(layero, index){
+                cancel: function (layero, index) {
                     layer.closeAll()
                 }
             });
         }
 
-        $('.btn-open-imglib').on('click', function(){
-            showChoseImageDialog("","/admin/material/imglib");
+        function wexPreview() {
+            if(saveAll('preview')){
+
+                //示范一个公告层
+                layer.open({
+                    type: 1
+                    ,
+                    title: false //不显示标题栏
+                    ,
+                    closeBtn: false
+                    ,
+                    area: '300px;'
+                    ,
+                    shade: 0.8
+                    ,
+                    id: 'LAY_layuipro' //设定一个id，防止重复弹出
+                    ,
+                    btn: ['火速围观', '残忍拒绝']
+                    ,
+                    moveType: 1 //拖拽模式，0或者1
+                    ,
+                    content: $('#wex-preview').html()
+                    ,
+                    success: function (layero) {
+                        var btn = layero.find('.layui-layer-btn');
+                        btn.css('text-align', 'center');
+                        btn.find('.layui-layer-btn0').attr({
+                            href: 'http://www.layui.com/'
+                            , target: '_blank'
+                        });
+                    }
+                });
+            }
+        }
+
+        // 添加项目
+        $('.articles-preview-container').on('click', '.add-new-item', function () {
+            var $parentItem = $(this).closest('.article-preview-item');
+            var $id = (new Date).getTime();
+            var $item = $($previewItemTemplate({item: {'cover_url': '/image/slt.png'}})).prop('id', $id);
+
+            $parentItem.before($item);
+
+            Article.add($id)
+
+            performAddBtn();
+        });
+
+        // 编辑项目
+        $('.articles-preview-container').on('click', 'a.edit', function () {
+
+            var $item = $(this).closest('.article-preview-item');
+
+            if ($item.hasClass('active')) {
+                return;
+            }
+            ;
+
+            var $article = Article.get($item.prop('id'));
+            console.log('form' + $item.prop('id') + "被编辑，内容为" + $article);
+            console.log($article);
+            $item.addClass('active').siblings().removeClass('active');
+
+            renderForm($article);
+        });
+
+        // 删除项目
+        $('.articles-preview-container').on('click', 'a.delete', function () {
+            var $item = $(this).closest('.article-preview-item');
+
+            Article.delete($item.prop('id'));
+
+            $item.slideUp(200, function () {
+                $(this).remove();
+
+                performAddBtn();
+            });
+
+            window.location.reload();
+        });
+
+        var $articles = Article.all();
+
+        //初始化除了第一张的其他图片
+        for ($id in $articles) {
+            if ($id == 'article-first') {
+                continue;
+            }
+
+            if (!$articles[$id].cover_url) {
+                $articles[$id].cover_url = '/image/slt.png';
+            }
+            $firstItem.after($($previewItemTemplate({item: $articles[$id]})).prop('id', $id));
+        }
+
+        $form.on('keyup', saveForm);
+        $ue.addListener('keyup', saveForm);
+        $('.btn-save').on('click', saveAll);
+        $('.btn-preview').on('click', wexPreview);
+
+        $('.btn-open-imglib').on('click', function () {
+            showChoseImageDialog("", "/admin/material/imglib");
         });
 
     });
--- resources/views/admin/material/new-article.blade.php
+++ resources/views/admin/material/new-article.blade.php
@@ -1,92 +1,172 @@
 @extends('admin.layout')
 @include('vendor.ueditor.assets')
 @section('content')
-<div class="console-content">
-    <div class="page-header">
-        <h2 id="nav">素材管理 - 新建图文 <a href="{{ admin_url('material') }}" class="btn btn-success btn-sm">返回素材列表</a></h2>
-    </div>
-    <div class="well row">
-        <div class="col-md-4">
-            <div class="articles-preview-container">
-                <div class="article-preview-item first" id="article-first">
-                    <div class="article-preview-item-cover-placeholder">封面图片</div>
-                    <div class="article-preview-item-title attr-title">标题</div>
-                    <div class="article-preview-item-edit-links"><a href="javascript:;" class="edit"><i class="ion-edit"></i></a></div>
-                </div>
+    <div class="console-content">
+        <div class="page-header">
+            <h2 id="nav">素材管理 - 新建图文 <a href="{{ admin_url('material') }}" class="btn btn-success btn-sm">返回素材列表</a>
+            </h2>
+        </div>
+        <div class="well row">
+            <div class="col-md-4">
+                <div class="articles-preview-container">
+                    <div class="article-preview-item first" id="article-first">
+                        <div class="article-preview-item-cover-placeholder">封面图片</div>
+                        <div class="article-preview-item-title attr-title">标题</div>
+                        <div class="article-preview-item-edit-links"><a href="javascript:;" class="edit"><i
+                                        class="ion-edit"></i></a></div>
+                    </div>
 
-                <div class="article-preview-item button-box">
-                    <a href="javascript:;" class="add-new-item"><i class="ion-plus"></i></a>
+                    <div class="article-preview-item button-box">
+                        <a href="javascript:;" class="add-new-item"><i class="ion-plus"></i></a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-8">
-            <form action="" method="POST" role="form" class="article-form">
-                <div class="form-group">
-                    <label>标题</label>
-                    <div class="alert alert-warning alert-dismissible article-title" role="alert" style="display: none">
-                        <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                        <strong>Warning!</strong> 请添加文章标题
+            <div class="col-md-8">
+                <form action="" method="POST" role="form" class="article-form">
+                    <div class="form-group">
+                        <label>标题</label>
+                        <div class="alert alert-warning alert-dismissible article-title" role="alert"
+                             style="display: none">
+                            <button type="button" class="close" data-dismiss="alert"><span
+                                        aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                            <strong>Warning!</strong> 请添加文章标题
+                        </div>
+                        <input type="text" name="title" id="input" class="form-control" value="" required="required"
+                               title="">
                     </div>
-                    <input type="text" name="title" id="input" class="form-control" value="" required="required"  title="">
-                </div>
-                <div class="form-group">
-                    <label>作者<small>（选填）</small></label>
-                    <input type="text" name="author" id="input" class="form-control" value="" required="required"  title="">
-                </div>
-
-                <div class="form-group">
-                    <label>摘要<small>（选填，该摘要只在发送图文消息为单条时显示）</small></label>
-                    <textarea name="description" id="inputContent" class="form-control" rows="3"></textarea>
-                </div>
-
-                <div class="form-group form-group-div">
-                    <div class="alert alert-warning alert-dismissible article-cover_url" role="alert"  style="display: none">
-                        <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                        <strong>Warning!</strong> 请选择一个封面
-                    </div>
-                    <label>封面<small>（小图片建议尺寸：200像素 * 200像素）</small></label>
-                    <div>
-                        {{--<button type="button" class="btn btn-light">上传</button>--}}
-                        <input type="input" name="cover_url" id="cover_url" value="">
-                        <button type="button" class="btn btn-open-imglib btn-light">从图片库选择</button>
-                        <label>
-                            <input type="checkbox" name="show_cover_pic" value="" class="js-switch" data-size="small">
-                            封面图片显示在正文中
+                    <div class="form-group">
+                        <label>作者
+                            <small>（选填）</small>
                         </label>
+                        <input type="text" name="author" id="input" class="form-control" value="" required="required"
+                               title="">
                     </div>
-                    <img src="" alt="">
-                </div>
 
-                <div class="form-group">
-                    <div class="alert alert-warning alert-dismissible article-content" role="alert"  style="display: none">
-                        <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                        <strong>Warning!</strong> 请编写文章内容
+                    <div class="form-group">
+                        <label>摘要
+                            <small>（选填，该摘要只在发送图文消息为单条时显示）</small>
+                        </label>
+                        <textarea name="description" id="inputContent" class="form-control" rows="3"></textarea>
                     </div>
-                    <label>文章内容<small></small></label>
-                    <script id="container" name="content" style="width:100%;height:350px;" type="text/template"></script>
-                </div>
 
-                <div class="form-group">
-                    <label>原文链接<small></small></label>
-                    <input type="text" name="source_url" id="input" class="form-control" value="" required="required"  title="">
-                </div>
-            </form>
-        </div>
-        <div class="col-md-12 text-center">
-            <hr>
-            <input type="hidden" name="cover_media_id">
-            <input type="hidden" name="cover_url">
-            <button type="submit" class="btn btn-primary btn-save">保 存</button>
-            <button type="submit" class="btn btn-default btn-preview">预 览</button>
-            <button type="submit" class="btn btn-default btn-save-send btn-success">保存并发送</button>
+                    <div class="form-group form-group-div">
+                        <div class="alert alert-warning alert-dismissible article-cover_url" role="alert"
+                             style="display: none">
+                            <button type="button" class="close" data-dismiss="alert"><span
+                                        aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                            <strong>Warning!</strong> 请选择一个封面
+                        </div>
+                        <label>封面
+                            <small>（小图片建议尺寸：200像素 * 200像素）</small>
+                        </label>
+                        <div>
+                            {{--<button type="button" class="btn btn-light">上传</button>--}}
+                            <input type="input" name="cover_url" id="cover_url" value="">
+                            <button type="button" class="btn btn-open-imglib btn-light">从图片库选择</button>
+                            <label>
+                                <input type="checkbox" name="show_cover_pic" value="" class="js-switch"
+                                       data-size="small">
+                                封面图片显示在正文中
+                            </label>
+                        </div>
+                        <img src="" alt="">
+                    </div>
+
+                    <div class="form-group">
+                        <div class="alert alert-warning alert-dismissible article-content" role="alert"
+                             style="display: none">
+                            <button type="button" class="close" data-dismiss="alert"><span
+                                        aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                            <strong>Warning!</strong> 请编写文章内容
+                        </div>
+                        <label>文章内容
+                            <small></small>
+                        </label>
+                        <script id="container" name="content" style="width:100%;height:350px;"
+                                type="text/template"></script>
+                    </div>
+
+                    <div class="form-group">
+                        <label>原文链接
+                            <small></small>
+                        </label>
+                        <input type="text" name="source_url" id="input" class="form-control" value=""
+                               required="required" title="">
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-12 text-center">
+                <hr>
+                <input type="hidden" name="cover_media_id">
+                <input type="hidden" name="cover_url">
+                <button type="submit" class="btn btn-primary btn-save">保 存</button>
+                <button type="submit" class="btn btn-default btn-preview">预 览</button>
+                <button type="submit" class="btn btn-default btn-save-send btn-success">保存并发送</button>
+            </div>
         </div>
     </div>
-</div>
-<div id="current_"></div>
+    <div id="current_"></div>
 
-<script type="text/template" id="preview-item-template">
-    <div class="article-preview-item deleteable">
-        <div class="article-preview-item-thumb-title attr-title"><%= item.title || '标题' %></div>
+    <div id="wex-preview">
+        <div class="wx_phone_preview">
+            <div class="wx_phone">
+                <div class="wx_phone_hd">
+                    王继超
+                </div>
+                <div class="wx_phone_bd">
+                    <div class="wx_phone_preview_appmsg appmsg_wap">
+                        <div class="rich_media">
+                            <div class="rich_media_area_primary">
+                                <h2 class="rich_media_title" title="那一年，你还记得吗">那一年，你还记得吗</h2>
+                                <div class="rich_media_meta_list">
+                                    <!-- <span class="rich_media_meta meta_original_tag dn">原创</span>
+                                    <a class="rich_media_meta meta_enterprise_tag" href="javascript:;"><img src="http://mmbiz.qpic.cn/mmbiz/sHCMyzurWv7hBXKI1vsotnuSWZxSu2QicqJy5ygBCWRSktDVbfnUrfg6A6fJIc8M0hORTWibPFKMHibKKMaBibQKTg/0"></a> -->
+                                    <em class="rich_media_meta rich_media_meta_text">2015-04-16</em>
+                                    <em class="rich_media_meta rich_media_meta_text"></em>
+                                    <span class="rich_media_meta rich_media_meta_link" title="请发送到手机查看完整效果">王继超</span>
+                                </div>
+
+                                <div class="rich_media_thumb_wrp">
+                                    <img src="http://mmbiz.qpic.cn/mmbiz/sHCMyzurWv7hBXKI1vsotnuSWZxSu2QicqJy5ygBCWRSktDVbfnUrfg6A6fJIc8M0hORTWibPFKMHibKKMaBibQKTg/0"
+                                         class="rich_media_thumb" onerror="this.parentNode.removeChild(this)">
+                                </div>
+
+                                <div class="rich_media_content">
+                                    <p>这个多雨的城市，留下了什么？你还记得吗？</p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="wx_view_container jsPhoneViewPlugin">
+                <div>
+                    <ul class="wx_view_list">
+                        <li class="wx_view_item jsPhoneViewLink" data-id="card">图文消息</li>
+                        <li class="wx_view_item jsPhoneViewLink selected" data-id="appmsg">消息正文</li>
+                        <li class="wx_view_item jsPhoneViewLink" data-id="moments">分享到朋友圈</li>
+                        <li class="wx_view_item jsPhoneViewLink" data-id="chat">发送给朋友</li>
+                    </ul>
+
+                    <ul class="wx_article_crtl">
+                        <li class="crtl_btn crtl_pre_btn  jsPhoneViewCard" data-index="0">上一篇</li>
+                        <li class="crtl_btn crtl_next_btn  jsPhoneViewCard" data-index="2">下一篇</li>
+                    </ul>
+
+                    <div class="btn_wx_phone_preview_wrp">
+                        <a class="btn btn_default btn_wx_phone_preview jsPhoneViewPub">发送到手机预览</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <script type="text/template" id="preview-item-template">
+        <div class="article-preview-item deleteable">
+            <div class="article-preview-item-thumb-title attr-title"><%= item.title || '标题' %></div>
         <div class="article-preview-item-thumb-img"><img src="<%= item.cover_url %>" alt="<%= item.title %>"></div>
         <div class="article-preview-item-edit-links"><a href="javascript:;" class="edit"><i class="ion-edit"></i></a><a href="javascript:;" class="delete"><i class="ion-trash-a"></i></a></div>
     </div>
